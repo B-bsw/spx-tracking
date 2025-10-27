b962cb4ba5fd7c047ecfefd3f82dc19d66ce6bba
BB <sorawitthassadorn15@gmail.com>
2025-10-27 20:56:41 +0700

    first commit

---
+++ src/app/api/route.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export async function GET(request) {
+  try {
+    const url = new URL(request.url);
+    const spx_tn = url.searchParams.get("spx_tn");
+
+    const res = await axios.get(
+      `https://spx.co.th/shipment/order/open/order/get_order_info?spx_tn=${spx_tn}&language_code=th`
+    );
+
+    return Response.json(res.data);
+  } catch (error) {
+    console.log(error);
+    return Response.json(error);
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,134 @@
+"use client";
 import Image from "next/image";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+type TrackingRecord = {
+    time: string;
+    status: string;
+    description: string;
+};
+
+type Data = {
+    fulfillment_info: {
+        deliver_type: number;
+    };
+    is_instant_order: boolean;
+    sls_tracking_info: {
+        sls_tn: string;
+        client_order_id: string;
+        receiver_name: string;
+        records: {
+            time: string;
+            status: string;
+            description: string;
+            buyer_description: string;
+            tracking_code: string;
+            current_location: {
+                full_address: string;
+                lat: string;
+                lng: string;
+            };
+        }[];
+    };
+};
+
+export default function Page() {
+    const [txtInput, setTxtInput] = useState<string | null>(null);
+    const [txt, setTxt] = useState<Data | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    useEffect(() => {
+        axios
+            .get("/api")
+            .then((res) => {
+                setTxt(res.data.data);
+                setLoading(false);
+            })
+            .catch(console.error);
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="h-screen bg-zinc-100 flex items-center justify-center">
+                <div>loading...</div>
+            </div>
+        );
+    }
+
+    const handleChangeTxt = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTxtInput(event.target.value.toUpperCase());
+    };
+
+    const handleSearch = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        axios
+            .get(`/api?spx_tn=${txtInput}`)
+            .then((res) => {
+                console.log(res.data.data);
+                setTxt(res.data.data);
+                setLoading(false);
+            })
+            .catch(console.error);
+    };
+
+    return (
+        <div className="h-screen flex flex-col justify-center items-center gap-5">
+            <div className="grid grid-cols-1 gap-4 h-full">
+                <div className="m-4">
+                    <form onSubmit={handleSearch}>
+                        <div className="flex p-6 rounded-2xl shadow-md items-center justify-center gap-2">
+                            <div className="font-medium w-fit text-nowrap">
+                                กรอกเลขพัสดุ
+                            </div>
+                            <input
+                                type="text"
+                                onChange={handleChangeTxt}
+                                className="outline-2 outline-zinc-200 focus:outline-black rounded-sm p-1 w-full"
+                            />
+                        </div>
+                    </form>
+                </div>
+                {txt ? (
+                    txt.sls_tracking_info.records.map((items, index) => (
+                        <div
+                            className="p-6 shadow-md rounded-2xl flex flex-col gap-4"
+                            key={index}
+                        >
+                            <div>
+                                <span className="font-bold">Status:</span>{" "}
+                                {items.buyer_description}
+                            </div>
+                            {items.current_location.full_address && (
+                                <div>
+                                    <span className="font-bold">Address:</span>{" "}
+                                    {items.current_location.full_address}
+                                </div>
+                            )}
+                            {items.current_location.lat.length > 0 && (
+                                <div>
+                                    <Link
+                                        className="bg-zinc-100 rounded-md p-1"
+                                        target="_blank"
+                                        href={`https://www.google.com/maps/search/?api=1&query=${
+                                            items.current_location.lat +
+                                            " " +
+                                            items.current_location.lng
+                                        }`}
+                                    >
+                                        <span className="text-blue-600 underline">
+                                            URL ADDRESS:{" "}
+                                        {items.current_location.full_address}
+                                        </span>
+                                    </Link>
+                                </div>
+                            )}
+                        </div>
+                    ))
+                ) : (
+                    <div className="text-center font-bold text-5xl">no data</div>
+                )}
+            </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    );
 }
